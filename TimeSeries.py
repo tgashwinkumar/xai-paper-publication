e484d3aec29b3f8fcceebfbba2db2867724831c9
tgashwinkumar <tgashwinkumar@gmail.com>
2021-12-08 15:43:02 +0530

    Updated README.md

---
+++ TimeSeries.py
@@ -0,0 +1,9 @@
+
+from dateutil.parser import parse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+
+plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
